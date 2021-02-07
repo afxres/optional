ed147287520870fb9243626196f436df63eb0857
miko <stdarg@outlook.com>
2021-02-07 13:00:10 +0800

    Add unit tests for exception types

---
+++ code/Optional.Tests/OptionExceptionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace Mikodev.Optional.Tests
+{
+    public class OptionExceptionTests
+    {
+        [Fact(DisplayName = "New")]
+        public void New()
+        {
+            _ = new OptionException();
+        }
+
+        [Fact(DisplayName = "New With Message")]
+        public void NewWithMessage()
+        {
+            var error = new OptionException("Message context");
+            Assert.Equal("Message context", error.Message);
+        }
+
+        [Fact(DisplayName = "New With Inner Exception")]
+        public void NewWithInnerException()
+        {
+            var inner = new ArgumentException();
+            var error = new OptionException("Message", inner);
+            Assert.Equal("Message", error.Message);
+            Assert.True(ReferenceEquals(inner, error.InnerException));
+        }
+    }
+}
---
+++ code/Optional.Tests/ResultExceptionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace Mikodev.Optional.Tests
+{
+    public class ResultExceptionTests
+    {
+        [Fact(DisplayName = "New")]
+        public void New()
+        {
+            _ = new ResultException();
+        }
+
+        [Fact(DisplayName = "New With Message")]
+        public void NewWithMessage()
+        {
+            var error = new ResultException("Message context");
+            Assert.Equal("Message context", error.Message);
+        }
+
+        [Fact(DisplayName = "New With Inner Exception")]
+        public void NewWithInnerException()
+        {
+            var inner = new ArgumentException();
+            var error = new ResultException("Message", inner);
+            Assert.Equal("Message", error.Message);
+            Assert.True(ReferenceEquals(inner, error.InnerException));
+        }
+    }
+}
