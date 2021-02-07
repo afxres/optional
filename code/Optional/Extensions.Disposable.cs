779cd510cb5996d33e440790d8ea02362a965a61
miko <stdarg@outlook.com>
2021-02-07 13:34:26 +0800

    Remove disposable type

--- code/Optional/Extensions.Disposable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Mikodev.Optional
-{
-    public static partial class Extensions
-    {
-        public static Disposable Disposable(Action action) => new Disposable(action);
-    }
-}
